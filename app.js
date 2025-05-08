ddb7bba61fcd88bfeb292a0584ea3ed57928aaa8
Svilyear <villiersteven@gmail.com>
2025-05-08 13:57:11 +0100

    new commit

--- app.js
+++ app.js
@@ -201,11 +201,11 @@ if (!fs.existsSync('uploads')) {
 
 // MySQL connection pool using Railway-provided ENV variables
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    DB_PORT:'3306',
+    DB_USER:'root',
+    DB_PASSWORD:'EMbJsOLDqrTEfMxwTAZkkspIJeDxuECy',
+    DB_NAME:'railway',
+    DB_HOST:'mysql.railway.internal',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
