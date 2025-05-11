b40da35cbe55cb66a33fa88e409275365aa64942
Svilyear <villiersteven@gmail.com>
2025-05-11 16:46:05 +0100

    new commit

--- database.sql
+++ database.sql
@@ -17,9 +17,9 @@ CREATE TABLE IF NOT EXISTS products (
 );
 
 
-DROP TABLE IF EXISTS order_items;
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS cart_items;
+-- DROP TABLE IF EXISTS order_items;
+-- DROP TABLE IF EXISTS orders;
+-- DROP TABLE IF EXISTS cart_items;
 -- Create orders table
 
 
