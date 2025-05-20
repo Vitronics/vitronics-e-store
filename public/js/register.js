7a9923d45ed2fd3bf552ab5602ea5d0729fc082d
Svilyear <villiersteven@gmail.com>
2025-05-20 21:33:58 +0100

    new commit

--- public/js/register.js
+++ public/js/register.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
         try{
-            const response = await fetch('http://localhost:3000/api/register', {
+            const response = await fetch('/api/register', {
                 method: 'POST',
                 headers:  {
                     'Content-Type': 'application/json'
